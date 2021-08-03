8ee6e5715545060af7149be4a69848a1f520571a
DavidAntonPeraza <david.anton.00@gmail.com>
2021-08-03 20:36:11 +0200

    table sql script fixed

--- SQL/Tables.sql
+++ SQL/Tables.sql
@@ -1,46 +1,55 @@
+drop database if exists HotelManagement;
+create database HotelManagement;
 use HotelManagement;
 
-drop table if exists Users;
-create table Users(
-    `id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
-    `password` VARCHAR(255) NOT NULL DEFAULT '',
-    `name` VARCHAR(255) NOT NULL DEFAULT '',
-    `role` int(11),
-    `id_hotel` int(11), 
-    primary key (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+drop table if exists material_used cascade;
+drop table if exists material_required cascade;
+drop table if exists revision cascade;
+drop table if exists material cascade;
+drop table if exists upgrade cascade;
+drop table if exists issue cascade;
+drop table if exists user cascade;
+drop table if exists hotel cascade;
 
-drop table if exists Hotels;
-create table Hotels(
-    `id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
+create table hotel(
+    `id` VARCHAR(255) NOT NULL DEFAULT '',
     `name` VARCHAR(255) NOT NULL DEFAULT '',
     `mail` VARCHAR(255) NOT NULL DEFAULT '',
     `phone` VARCHAR(255) NOT NULL DEFAULT '',
     primary key (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-drop table if exists Issues;
-create table Issues(
-    `id` VARCHAR(255) NOT NULL DEFAULT '',
+create table user(
+    `id` VARCHAR(128) NOT NULL DEFAULT '',
+    `password` VARCHAR(255) NOT NULL DEFAULT '',
+    `name` VARCHAR(255) NOT NULL DEFAULT '',
+    `role` int(11),
+    `id_hotel` VARCHAR(128) NOT NULL DEFAULT '',
+    primary key (`id`),
+    foreign key (`id_hotel`) references hotel (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+create table issue(
+    `id` VARCHAR(128) NOT NULL DEFAULT '',
     `room` VARCHAR(255) NOT NULL DEFAULT '',
     `title` VARCHAR(255) NOT NULL DEFAULT '',
     `description` text,
     `picture` VARCHAR(255) NOT NULL DEFAULT '',
     `status` int(11) NOT NULL DEFAULT 0,
-    `id_hotel` int(11) NOT NULL,
-    `reporter_id` int(11) NOT NULL,
+    `id_hotel` VARCHAR(128) NOT NULL DEFAULT '',
+    `id_reporter` VARCHAR(128) NOT NULL DEFAULT '',
     `category` varchar(255) NOT NULL,
     `subcategory`  text,
     `priority` int(11),
     `date` date not null,
     primary key (`id`),
-    foreign key (`id_hotel`) references Hotel (`id`),
-    foreign key (`reporter_id`) references Users (`id`),
+    foreign key (`id_hotel`) references hotel (`id`),
+    foreign key (`id_reporter`) references user (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-drop table if exists Materials;
-create table Materials(
-    `id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
+create table material(
+    `id` VARCHAR(128) NOT NULL DEFAULT '',
     `name` VARCHAR(255) NOT NULL DEFAULT '',
     `quantity` int(11) not null default 0,
     `price` float not null default 0.0,
@@ -48,29 +57,26 @@ create table Materials(
     primary key (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-drop table if exists MaterialsUsed;
-create table MaterialsUsed(
-    `id_issue` int(11) NOT NULL,
-    `id_material` int(11) NOT NULL,
+create table material_used(
+    `id_issue`VARCHAR(128) NOT NULL DEFAULT '',
+    `id_material` VARCHAR(128) NOT NULL DEFAULT '',
     `quantity` int(11) NOT NULL,
     primary key (`id_issue`, `id_material`),
-    foreign key (`id_issue`) references Issues (`id`),
-    foreign key (`id_material`) references Materials (`id`)
+    foreign key (`id_issue`) references issue (`id`),
+    foreign key (`id_material`) references material (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-drop table if exists MaterialsRequired;
-create table MaterialsRequired(
-    `id_issue` int(11) NOT NULL,
-    `id_material` int(11) NOT NULL,
+create table material_required(
+    `id_issue` VARCHAR(128) NOT NULL DEFAULT '',
+    `id_material` VARCHAR(128) NOT NULL DEFAULT '',
     `quantity` int(11) NOT NULL,
     primary key (`id_issue`, `id_material`),
-    foreign key (`id_issue`) references Issues (`id`),
-    foreign key (`id_material`) references Materials (`id`)
+    foreign key (`id_issue`) references issue (`id`),
+    foreign key (`id_material`) references material (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-drop table if exists Revision;
-create table Revision(
-    `id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
+create table revision(
+    `id` VARCHAR(128) NOT NULL DEFAULT '',
     `title` VARCHAR(255) NOT NULL DEFAULT '',
     `description` int(11) not null default 0,
     `date` date not null,
@@ -79,16 +85,96 @@ create table Revision(
     primary key (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-drop table if exists Upgrades;
-create table Upgrades(
-    `id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
+create table upgrade(
+    `id` VARCHAR(128) NOT NULL DEFAULT '',
     `title` VARCHAR(255) NOT NULL DEFAULT '',
-    `id_hotel` int(11) NOT NULL,
-    `reporter_id` int(11) NOT NULL,
+    `id_hotel` VARCHAR(128) NOT NULL DEFAULT '',
+    `id_reporter` VARCHAR(128) NOT NULL DEFAULT '',
     `description` int(11) not null default 0,
     `date` date not null,
     `status` int(11) NOT NULL DEFAULT 0,
     `room` VARCHAR(255) NOT NULL DEFAULT '',
     `picture` VARCHAR(255) NOT NULL DEFAULT '',
-    primary key (`id`)
+    primary key (`id`),
+	foreign key (`id_hotel`) references hotel (`id`),
+    foreign key (`id_reporter`) references user (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+delimiter //
+DROP TRIGGER IF EXISTS before_insert_user//
+CREATE TRIGGER before_insert_user
+BEFORE INSERT ON user
+FOR EACH ROW
+BEGIN
+  IF new.id IS NULL
+	THEN
+		SET new.id = uuid();
+  END IF ;
+END;//
+delimiter ;
+
+delimiter //
+DROP TRIGGER IF EXISTS before_insert_hotel//
+CREATE TRIGGER before_insert_hotel
+BEFORE INSERT ON hotel
+FOR EACH ROW
+BEGIN
+  IF new.id IS NULL
+	THEN
+		SET new.id = uuid();
+  END IF ;
+END;//
+delimiter ;
+
+delimiter //
+DROP TRIGGER IF EXISTS before_insert_issue//
+CREATE TRIGGER before_insert_issue
+BEFORE INSERT ON issue
+FOR EACH ROW
+BEGIN
+  IF new.id IS NULL
+	THEN
+		SET new.id = uuid();
+  END IF ;
+END;//
+delimiter ;
+
+delimiter //
+DROP TRIGGER IF EXISTS before_insert_material//
+CREATE TRIGGER before_insert_material
+BEFORE INSERT ON material
+FOR EACH ROW
+BEGIN
+  IF new.id IS NULL
+	THEN
+		SET new.id = uuid();
+  END IF ;
+END;//
+delimiter ;
+
+delimiter //
+DROP TRIGGER IF EXISTS before_insert_revision//
+CREATE TRIGGER before_insert_revision
+BEFORE INSERT ON revision
+FOR EACH ROW
+BEGIN
+  IF new.id IS NULL
+	THEN
+		SET new.id = uuid();
+  END IF ;
+END;//
+delimiter ;
+
+delimiter //
+DROP TRIGGER IF EXISTS before_insert_upgrade//
+CREATE TRIGGER before_insert_upgrade
+BEFORE INSERT ON upgrade
+FOR EACH ROW
+BEGIN
+  IF new.id IS NULL
+	THEN
+		SET new.id = uuid();
+  END IF ;
+END;//
+delimiter ;
+
